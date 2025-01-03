62f3ed81011d0341e8c6e0768d0ebc830ab539a6
wangshaoshuai <649896433@qq.com>
2025-01-03 15:50:43 +0800

    add a hello world

---
+++ hello.py
@@ -0,0 +1,7 @@
+'''
+Date                : 2025-01-03 15:47:58
+LastEditors         : 王少帅
+LastEditTime        : 2025-01-03 15:47:58
+FilePath            : \FirstProject\hello.py
+'''
+print("Hello World!")
